9c6c5edc60063ed1a20ad3263ff4f09ec40cd27c
Jonathan Galentine <j.galentine@gmail.com>
2017-07-11 16:50:03 -0600

    Quick Merge AddShaker (#2)
    * Add shaker class and tests. Revise Die class to support nullable FaceValue notion.
    
    * Quick fix variable name, add comments.

---
+++ RollWithIt/Shaker.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollWithIt
+{
+    /// <summary>
+    /// A dice shaker, ala Boggle.
+    /// </summary>
+    public class Shaker
+    {
+        public ICollection<Die> Dice { get; private set; }
+
+        /// <summary>
+        /// Create an empty dice shaker.
+        /// </summary>
+        public Shaker()
+        {
+            Dice = new List<Die>();
+        }
+
+        /// <summary>
+        /// Add a die to the shaker.
+        /// </summary>
+        /// <param name="die">Die.</param>
+        public void AddDie(Die die)
+        {
+            Dice.Add(die);
+        }
+
+        /// <summary>
+        /// Remove all dice from the shaker.
+        /// </summary>
+        public void Empty()
+        {
+            Dice.Clear();
+        }
+
+        /// <summary>
+        /// Roll dice inside the shaker.
+        /// </summary>
+        public void Shake()
+        {
+            foreach (Die die in Dice)
+            {
+                die.Roll();
+            }
+        }
+    }
+}
---
+++ RollWithItTests/ShakerTests.cs
@@ -0,0 +1,47 @@
+﻿using Xunit;
+using RollWithIt;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+
+namespace RollWithIt.Tests
+{
+    [ExcludeFromCodeCoverage]
+    public class ShakerTests
+    {
+        [Fact]
+        public void AddDieTest()
+        {
+            var generator = new MockRandom();
+            var die = new Die(generator);
+            var shaker = new Shaker();
+            shaker.AddDie(die);
+            Assert.True(shaker.Dice.Count == 1);
+        }
+
+        [Fact]
+        public void ShakeTest()
+        {
+            var generator = new MockRandom();
+            var die = new Die(generator);
+            var shaker = new Shaker();
+            shaker.AddDie(die);
+            shaker.Shake();
+            Assert.True(shaker.Dice.First().FaceValue == MockRandom.DEFAULT_VALUE);
+        }
+
+        [Fact]
+        public void EmptyTest()
+        {
+            var generator = new MockRandom();
+            var die = new Die(generator);
+            var shaker = new Shaker();
+            shaker.AddDie(die);
+            shaker.Empty();
+            Assert.True(shaker.Dice.Count == 0);
+        }
+    }
+}
